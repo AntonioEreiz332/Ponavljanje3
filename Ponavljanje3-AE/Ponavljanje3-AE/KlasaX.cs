023c838f5f14b378f4835b7f9b20bf7cf65482b8
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-10-17 23:37:20 +0200

    Add files via upload

---
+++ Ponavljanje3-AE/Ponavljanje3-AE/KlasaX.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje3_AE
+{
+    class KlasaX
+    {
+        int[] arr = new int[100];
+        int indeks;
+
+        public KlasaX(int[] arr, int indeks)
+        {
+            this.arr = arr;
+            this.indeks = indeks;
+        }
+
+        public int[] Arr { get => arr; set => arr = value; }
+        public int Indeks { get => indeks; set => indeks = value; }
+
+        public KlasaX()
+        {
+        }
+
+
+    }
+}
---
+++ Ponavljanje3-AE/Ponavljanje3-AE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje3_AE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX x = new KlasaX();
+            for (x.Indeks = 0; x.Indeks < 100; x.Indeks++)
+            {
+                x.Arr[x.Indeks] = x.Indeks + 1;
+            }
+            Console.WriteLine("50. broj u polju Arr = " + x.Arr[49]);
+            Console.ReadKey();
+        }
+    }
+}
